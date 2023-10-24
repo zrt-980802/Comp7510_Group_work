a2dbfdff58fe2219502a7d68edee2f15f851345d
29557 <295575817@qq.c>
2023-10-25 01:09:04 +0800

    # finish construction of storage

--- EndPoint/Data.py
+++ EndPoint/Data.py
@@ -6,65 +6,103 @@ import certifi
 import os
 
 from EndPoint.ForumData.CommentInfo import CommentInfo
-from EndPoint.ForumData.TopicInfo import TopicInfo
+from EndPoint.ForumData.PostInfo import PostInfo
 from EndPoint.ForumData.UserInfo import UserInfo
 
 os.environ['SSL_CERT_FILE'] = certifi.where()
 
 import firebase_admin
 from firebase_admin import db, storage
+from google.cloud.storage import transfer_manager
 
-cred_obj = firebase_admin.credentials.Certificate('json/comp_23458844.json')
+cred_obj = firebase_admin.credentials.Certificate('json/token.json')
 
 firebase_admin.initialize_app(cred_obj, {
     'databaseURL': 'https://comp7510-934b1-default-rtdb.asia-southeast1.firebasedatabase.app/',
-    'storageBucket': 'gs://comp7510-934b1.appspot.com'
+    'storageBucket': 'comp7510-934b1.appspot.com'
 })
 
 db_ref = db.reference('/server/data')
-# bucket = storage.bucket()
+bucket = storage.bucket()
 
 
-def to_json(data):
-    # return json.dumps(self, cls=MyEncoder, indent=4)
-    print(data.__dict__)
-    return json.dumps(data.__dict__)
+# def to_json(data):
+#     # return json.dumps(self, cls=MyEncoder, indent=4)
+#     print(data.__dict__)
+#     return json.dumps(data.__dict__)
 
 
 def setInfo(info):
-    db_ref.child(info.type_name).child(info.getId()).set(to_json(info))
+    data_ref = db_ref.child(info.type_name).child(info.getId())  # .set(to_json(info))
+    for data in info.__dict__.items():
+        data_ref.child(data[0]).set(data[1])
 
 
 def updateInfo(info):
     setInfo(info)
 
 
+# 下面四个getInfo函数返回dict
+
+
 def getUserInfoById(userId: str):
-    return json.loads(getInfo(userId, UserInfo.type_name), object_hook=UserInfo)
+    """
+    :param userId:
+    :return: dict
+    """
+    return getInfo(userId, UserInfo.type_name)
 
 
-def getTopicInfoById(userId: str):
-    return json.loads(getInfo(userId, TopicInfo.type_name), object_hook=TopicInfo)
+def getPostInfoById(postId: str):
+    """
+    :param postId:
+    :return: dict
+    """
+    return getInfo(postId, PostInfo.type_name)
 
 
-def getCommentInfoById(userId: str):
-    return json.loads(getInfo(userId, CommentInfo.type_name), object_hook=CommentInfo)
+def getCommentInfoById(commentId: str):
+    """
+    :param commentId:
+    :return: dict
+    """
+    return getInfo(commentId, CommentInfo.type_name)
 
 
 def getInfo(ID: str, type_name):
+    """
+    :param ID:
+    :type_name:
+    :return: dict
+    """
     return db_ref.child(type_name).child(ID).get()
 
 
 def uploadFile(filePath):
     fileId = uuid.uuid1()
-    return fileId
+    return upload_blob(filePath, fileId)
 
 
-def upload_blob(bucket_name, source_file_name, destination_blob_name):
-    bucket = storage.bucket(bucket_name)
-    blob = bucket.blob(destination_blob_name)
-    generation_match_precondition = 0
-    blob.upload_from_filename(source_file_name, if_generation_match=generation_match_precondition)
-    print(
-        f"File {source_file_name} uploaded to {destination_blob_name}."
+def upload_blob(filePath, fileId):
+    fileName = os.path.basename(filePath)
+    blob = bucket.blob(str(fileId) + '/' + fileName)
+    print(str(fileId) + '/' + fileName)
+    blob.upload_from_filename(filePath)
+    blob.make_public()
+    return blob.public_url
+
+# todo 完善多文件上传
+def upload_many_blobs_with_transfer_manager(filenames, source_directory="", processes=8
+):
+    results = transfer_manager.upload_many_from_filenames(
+        bucket, filenames, source_directory=source_directory, max_workers=processes
     )
+
+    for name, result in zip(filenames, results):
+        # The results list is either `None` or an exception for each filename in
+        # the input list, in order.
+
+        if isinstance(result, Exception):
+            print("Failed to upload {} due to exception: {}".format(name, result))
+        else:
+            print("Uploaded {} to {}.".format(name, bucket.name))
---
+++ EndPoint/ForumData/PostInfo.py
@@ -0,0 +1,29 @@
+import time
+
+from faker import Faker
+
+
+class PostInfo:
+    type_name = 'postInfo'
+    post_id: str = None
+    post_title: str = None
+    post_content: str = None
+    post_annex: list = None
+    post_state: int = None
+    post_create_time: str = None
+    post_creator_user_id: str = None
+
+    def __init__(self, mock=False):
+        faker = Faker()
+        if mock:
+            self.post_id = faker.uuid4()
+            self.post_title = faker.word()
+            self.post_content = faker.paragraph()
+            self.post_annex = [faker.url(), faker.url(), faker.url()]
+            self.post_state = faker.random_digit()
+            self.post_create_time = str(faker.date_time())
+            self.post_creator_user_id = faker.uuid4()
+        return
+
+    def getId(self):
+        return self.post_id
